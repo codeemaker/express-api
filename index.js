495f96ebc715eec586d6834fd45e5b658bff9361
codeemaker <jgopinathjayakumar@gmail.com>
2020-10-11 12:03:50 +0530

    api using express added

---
+++ index.js
@@ -0,0 +1,33 @@
+var express=require("express");
+var body=require("body-parser");
+var ap=require("path");
+var student=require("./student");
+var app=express();
+app.use(body.json());
+app.get("/",(req,res)=>{
+ //res.sendFile(ap.join("/data/data/com.termux/files/home/storage/shared/htdocs","login.html"));
+ res.json(student);
+});
+app.post("/login",(req,res)=>{
+  const user={
+    id : student.length+1,
+    name : req.body.name
+  }
+  student.push(user);
+  res.json(user);
+ });
+ app.put("/api/:id",(req,res)=>{
+   var index=student.findIndex((st)=>{
+     return (st.id===parseInt(req.params.id))
+   });
+   if(index>=0){
+     var std=student[index];
+     std.name=req.body.name;
+     res.json(std);
+   }
+   else{
+     res.status(400);
+     res.send(req.params.id+" "+index);
+   }
+ })
+.listen(8080);
---
+++ student.js
@@ -0,0 +1,12 @@
+const student=[
+{
+"id":1,
+"name":"monsac"
+},
+{
+"id":2,
+"name":"Elon"
+}
+];
+
+module.exports=student;
